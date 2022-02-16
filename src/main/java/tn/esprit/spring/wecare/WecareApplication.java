f1e0d5d656fee4245287a516f3f194551d50027a
MoallaAlaa <alaaabdessalem.moalla@esprit.tn>
2022-02-16 13:50:56 +0100

    Wecare

---
+++ src/main/java/tn/esprit/spring/wecare/WecareApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.wecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WecareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WecareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/spring/wecare/WecareApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.wecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WecareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
